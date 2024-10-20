561dd016cd61de5497ee749cd53812f2d13234bf
Wesley Luk <wesleyluk@gmail.com>
2024-10-20 11:32:18 -0700

    Working recenter

--- FfxivVr/Renderer.cs
+++ FfxivVr/Renderer.cs
@@ -35,12 +35,12 @@ unsafe internal class Renderer : IDisposable
         CreateReferenceSpace();
     }
 
-    private Posef currentSpacePose = new Posef(orientation: new Quaternionf(0, 0, 0, 1), position: new Vector3f(0, 0, 0));
+    private Matrix4X4<float> currentSpaceTransform = Matrix4X4<float>.Identity;
     private void CreateReferenceSpace()
     {
         var localSpaceCreateInfo = new ReferenceSpaceCreateInfo(
             referenceSpaceType: ReferenceSpaceType.Local,
-            poseInReferenceSpace: currentSpacePose
+            poseInReferenceSpace: GetCurrentPose()
         );
         xr.CreateReferenceSpace(system.Session, ref localSpaceCreateInfo, ref localSpace).CheckResult("CreateReferenceSpace");
         var viewSpaceCreateInfo = new ReferenceSpaceCreateInfo(
@@ -237,14 +237,29 @@ unsafe internal class Renderer : IDisposable
     {
         logger.Info("Resetting camera");
         var oldSpace = localSpace;
-        currentSpacePose = new Posef(orientation: new Quaternionf(0, 0, 0, 1), position: new Vector3f(0, 0, 0));
+        currentSpaceTransform = Matrix4X4<float>.Identity;
+        Posef pose = GetCurrentPose();
         localSpace = new Space();
         var localSpaceCreateInfo = new ReferenceSpaceCreateInfo(
             referenceSpaceType: ReferenceSpaceType.Local,
-            poseInReferenceSpace: currentSpacePose
+            poseInReferenceSpace: pose
         );
         xr.CreateReferenceSpace(system.Session, ref localSpaceCreateInfo, ref localSpace).CheckResult("CreateReferenceSpace");
         xr.DestroySpace(oldSpace).CheckResult("DestroySpace");
+    }
+
+    private Posef GetCurrentPose()
+    {
+        Vector3D<float> scale;
+        Vector3D<float> translation;
+        Quaternion<float> rotation;
+        Matrix4X4.Decompose(currentSpaceTransform, out scale, out rotation, out translation);
+
+        var pose = new Posef(
+            orientation: rotation.ToQuaternionf(),
+            position: translation.ToVector3f()
+        );
+        return pose;
     }
 
     internal void RecenterCamera()
@@ -253,11 +268,20 @@ unsafe internal class Renderer : IDisposable
         var spaceLocation = new SpaceLocation(next: null);
         xr.LocateSpace(viewSpace, localSpace, lastTime, ref spaceLocation).CheckResult("LocateSpace");
         var oldSpace = localSpace;
-        currentSpacePose.Position = (currentSpacePose.Position.ToVector3D() + spaceLocation.Pose.Position.ToVector3D()).ToVector3f();
+        var pose = spaceLocation.Pose;
+        var positionMatrix = Matrix4X4.CreateTranslation(pose.Position.ToVector3D());
+        var newRotation = pose.Orientation.ToQuaternion();
+        newRotation.X = 0;
+        newRotation.Z = 0;
+        newRotation = Quaternion<float>.Normalize(newRotation);
+        var rotationMatrix = Matrix4X4.CreateFromQuaternion(newRotation);
+
+        currentSpaceTransform = rotationMatrix * positionMatrix * currentSpaceTransform;
+
         localSpace = new Space();
         var localSpaceCreateInfo = new ReferenceSpaceCreateInfo(
             referenceSpaceType: ReferenceSpaceType.Local,
-            poseInReferenceSpace: currentSpacePose
+            poseInReferenceSpace: GetCurrentPose()
         );
         xr.CreateReferenceSpace(system.Session, ref localSpaceCreateInfo, ref localSpace).CheckResult("CreateReferenceSpace");
         xr.DestroySpace(oldSpace).CheckResult("DestroySpace");
