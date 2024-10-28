00574fd82bb74a24ecccadb73b4e1fe491136519
Wesley Luk <wesleyluk@gmail.com>
2024-10-27 21:19:39 -0700

    Split game visibility

--- FfxivVr/GameVisibility.cs
+++ FfxivVr/GameVisibility.cs
@@ -2,16 +2,9 @@
 using Dalamud.Game.ClientState.Objects.Types;
 using Dalamud.Plugin.Services;
 using FFXIVClientStructs.FFXIV.Client.Game.Character;
-using FFXIVClientStructs.FFXIV.Client.Game.Control;
 using FFXIVClientStructs.FFXIV.Client.Game.Object;
 using FFXIVClientStructs.FFXIV.Client.Graphics.Scene;
-using FFXIVClientStructs.FFXIV.Client.System.Framework;
-using FFXIVClientStructs.FFXIV.Client.System.Memory;
 using FFXIVClientStructs.FFXIV.Client.UI;
-using FFXIVClientStructs.FFXIV.Component.GUI;
-using System.Collections.Generic;
-using System.Runtime.InteropServices;
-using static FFXIVClientStructs.FFXIV.Client.UI.AddonNamePlate;
 
 namespace FfxivVR;
 unsafe internal class GameVisibility
@@ -21,6 +14,8 @@ unsafe internal class GameVisibility
     private readonly IGameGui gameGui;
     private readonly ITargetManager targetManager;
     private readonly IClientState clientState;
+    private readonly NameplateModifier nameplateModifier;
+    private readonly SkeletonModifier skeletonModifier;
 
     public enum ModelCullTypes : byte
     {
@@ -37,8 +32,9 @@ unsafe internal class GameVisibility
         this.gameGui = gameGui;
         this.targetManager = targetManager;
         this.clientState = clientState;
+        this.nameplateModifier = new NameplateModifier(logger, gameGui, targetManager);
+        this.skeletonModifier = new SkeletonModifier(logger);
     }
-
 
     public void ForceFirstPersonBodyVisible()
     {
@@ -154,275 +150,11 @@ unsafe internal class GameVisibility
             return;
         }
         var skeleton = characterBase->Skeleton;
-        if (skeleton == null)
-        {
-            return;
-        }
-        var neckChildren = new List<int>();
-        int? neckIndex = null;
-        if (skeleton->SkeletonResourceHandles != null && skeleton->SkeletonResourceHandles[0] != null)
-        {
-            var havokSkeleton = skeleton->SkeletonResourceHandles[0]->HavokSkeleton;
-            for (int currentBone = 0; currentBone < havokSkeleton->Bones.Length; currentBone++)
-            {
-                var bone = havokSkeleton->Bones[currentBone];
-                var parent = havokSkeleton->ParentIndices[currentBone];
-                var name = bone!.Name!.String;
-                if (name == "j_kubi")
-                {
-                    neckIndex = currentBone;
-                }
-                if (parent == neckIndex)
-                {
-                    neckChildren.Add(currentBone);
-                }
-            }
-        }
-        if (neckIndex != null)
-        {
-            for (int i = 0; i < skeleton->PartialSkeletonCount; i++)
-            {
-                var partial = skeleton->PartialSkeletons[i].GetHavokPose(0);
-                if (partial == null)
-                {
-                    continue;
-                }
-                if (neckIndex >= partial->LocalPose.Length)
-                {
-                    continue;
-                }
-                var transform = partial->LocalPose[(int)neckIndex!];
-                transform.Scale.X = 0.00001f;
-                transform.Scale.Y = 0.00001f;
-                transform.Scale.Z = 0.00001f;
-                transform.Scale.W = 0;
-                partial->LocalPose[(int)neckIndex!] = transform;
-
-                neckChildren.ForEach((c) =>
-                {
-                    if (c < partial->LocalPose.Length)
-                    {
-                        var transform = partial->LocalPose[(int)c];
-                        transform.Translation.X *= -1;
-                        transform.Translation.Y *= -1;
-                        transform.Translation.Z *= -1;
-                        transform.Translation.W *= -1;
-                        transform.Scale.X = 0.00001f;
-                        transform.Scale.Y = 0.00001f;
-                        transform.Scale.Z = 0.00001f;
-                        transform.Scale.W = 0.00001f;
-                        partial->LocalPose[(int)c] = transform;
-                    }
-                });
-            }
-        }
-        else
-        {
-            logger.Debug("Neck bone not found");
-        }
+        skeletonModifier.HideHead(skeleton);
     }
 
     internal void UpdateNamePlates(AddonNamePlate* namePlate)
     {
-        //----
-        // Disables the target arrow until it can be put in the world
-        //----
-        AtkUnitBase* targetAddon = (AtkUnitBase*)gameGui!.GetAddonByName("_TargetCursor", 1);
-        if (targetAddon != null)
-        {
-            targetAddon->Alpha = 1;
-            targetAddon->Hide(true, false, 0);
-            //targetAddon->RootNode->SetUseDepthBasedPriority(true);
-        }
-
-        fixed (AtkTextNode** pvrTargetCursor = &vrTargetCursor)
-            SetupVRTargetCursor(pvrTargetCursor, 10);
-
-        for (byte i = 0; i < 50; i++)
-        {
-            NamePlateObject* npObj = &namePlate->NamePlateObjectArray[i];
-            AtkComponentBase* npComponent = npObj->RootComponentNode->Component;
-
-            for (int j = 0; j < npComponent->UldManager.NodeListCount; j++)
-            {
-                AtkResNode* child = npComponent->UldManager.NodeList[j];
-                child->SetUseDepthBasedPriority(true);
-            }
-
-            npObj->RootComponentNode->Component->UldManager.UpdateDrawNodeList();
-        }
-
-        NamePlateObject* selectedNamePlate = null;
-        var framework = Framework.Instance();
-        UI3DModule* ui3DModule = framework->GetUIModule()->GetUI3DModule();
-
-        IGameObject targObj = targetManager.Target!;
-        if (targObj != null)
-        {
-            TargetSystem* targSys = (TargetSystem*)targObj.Address;
-            for (int i = 0; i < ui3DModule->NamePlateObjectInfoCount; i++)
-            {
-                UI3DModule.ObjectInfo* objectInfo = ui3DModule->NamePlateObjectInfoPointers[i];
-                if (objectInfo->GameObject == targSys->Target)
-                {
-                    selectedNamePlate = &namePlate->NamePlateObjectArray[objectInfo->NamePlateIndex];
-                    break;
-                }
-            }
-        }
-
-        fixed (AtkTextNode** pvrTargetCursor = &vrTargetCursor)
-        {
-            UpdateVRCursorSize(pvrTargetCursor, 10);
-            SetVRCursor(pvrTargetCursor, selectedNamePlate);
-        }
-    }
-    private AtkTextNode* vrTargetCursor = null;
-    private NamePlateObject* currentNPTarget = null;
-
-    public unsafe bool SetupVRTargetCursor(AtkTextNode** vrTrgCursor, int targetCursorSize)
-    {
-        if ((*vrTrgCursor) != null)
-        {
-            return true;
-        }
-
-        (*vrTrgCursor) = (AtkTextNode*)Marshal.AllocHGlobal(sizeof(AtkTextNode));
-        if ((*vrTrgCursor) == null)
-        {
-            logger.Info("Failed to allocate memory for text node");
-            return false;
-        }
-        IMemorySpace.Memset((*vrTrgCursor), 0, (ulong)sizeof(AtkTextNode));
-        (*vrTrgCursor)->Ctor();
-
-        (*vrTrgCursor)->AtkResNode.Type = NodeType.Text;
-        (*vrTrgCursor)->AtkResNode.NodeFlags = NodeFlags.UseDepthBasedPriority;
-        (*vrTrgCursor)->AtkResNode.DrawFlags = 12;
-
-        (*vrTrgCursor)->LineSpacing = 12;
-        (*vrTrgCursor)->AlignmentFontType = 4;
-        (*vrTrgCursor)->FontSize = (byte)targetCursorSize;
-        (*vrTrgCursor)->TextFlags = (byte)(TextFlags.AutoAdjustNodeSize | TextFlags.Edge);
-        (*vrTrgCursor)->TextFlags2 = 0;
-
-        (*vrTrgCursor)->SetText("↓");
-
-        (*vrTrgCursor)->AtkResNode.ToggleVisibility(true);
-
-        (*vrTrgCursor)->AtkResNode.SetPositionShort(90, -23);
-        ushort outWidth = 0;
-        ushort outHeight = 0;
-        (*vrTrgCursor)->GetTextDrawSize(&outWidth, &outHeight);
-        (*vrTrgCursor)->AtkResNode.SetWidth((ushort)(outWidth));
-        (*vrTrgCursor)->AtkResNode.SetHeight((ushort)(outHeight));
-
-        // white fill
-        (*vrTrgCursor)->TextColor.R = 255;
-        (*vrTrgCursor)->TextColor.G = 255;
-        (*vrTrgCursor)->TextColor.B = 255;
-        (*vrTrgCursor)->TextColor.A = 255;
-
-        // yellow/golden glow
-        (*vrTrgCursor)->EdgeColor.R = 235;
-        (*vrTrgCursor)->EdgeColor.G = 185;
-        (*vrTrgCursor)->EdgeColor.B = 7;
-        (*vrTrgCursor)->EdgeColor.A = 255;
-
-        return true;
-    }
-
-    public void SetVRCursor(AtkTextNode** vrTrgCursor, NamePlateObject* nameplate)
-    {
-        // nothing to do!
-        if (currentNPTarget == nameplate)
-            return;
-
-        if ((*vrTrgCursor) != null)
-        {
-            if (currentNPTarget != null)
-            {
-                RemoveVRCursor(vrTrgCursor, currentNPTarget);
-                currentNPTarget = null;
-            }
-
-            if (nameplate != null)
-            {
-                AddVRCursor(vrTrgCursor, nameplate);
-                currentNPTarget = nameplate;
-            }
-        }
-    }
-    public void AddVRCursor(AtkTextNode** vrTrgCursor, NamePlateObject* nameplate)
-    {
-        if (nameplate != null && (*vrTrgCursor) != null)
-        {
-            var npComponent = nameplate->RootComponentNode->Component;
-
-            var lastChild = npComponent->UldManager.RootNode;
-            while (lastChild->PrevSiblingNode != null) lastChild = lastChild->PrevSiblingNode;
-
-            lastChild->PrevSiblingNode = (AtkResNode*)(*vrTrgCursor);
-            (*vrTrgCursor)->AtkResNode.NextSiblingNode = lastChild;
-            (*vrTrgCursor)->AtkResNode.ParentNode = (AtkResNode*)nameplate->RootComponentNode;
-
-            npComponent->UldManager.UpdateDrawNodeList();
-        }
-    }
-
-    public void RemoveVRCursor(AtkTextNode** vrTrgCursor, NamePlateObject* nameplate)
-    {
-        if (nameplate != null && (*vrTrgCursor) != null)
-        {
-            var npComponent = nameplate->RootComponentNode->Component;
-
-            var lastChild = npComponent->UldManager.RootNode;
-            while (lastChild->PrevSiblingNode != null) lastChild = lastChild->PrevSiblingNode;
-
-            if (lastChild == (*vrTrgCursor))
-            {
-                lastChild->NextSiblingNode->PrevSiblingNode = null;
-
-                (*vrTrgCursor)->AtkResNode.NextSiblingNode = null;
-                (*vrTrgCursor)->AtkResNode.ParentNode = null;
-
-                npComponent->UldManager.UpdateDrawNodeList();
-            }
-            else
-            {
-                logger.Error("RemoveVRCursor: lastChild != vrTargetCursor");
-            }
-        }
-    }
-    public void UpdateVRCursorSize(AtkTextNode** vrTrgCursor, int targetCursorSize)
-    {
-        if ((*vrTrgCursor) == null) return;
-
-        (*vrTrgCursor)->FontSize = (byte)targetCursorSize;
-        ushort outWidth = 0;
-        ushort outHeight = 0;
-        (*vrTrgCursor)->GetTextDrawSize(&outWidth, &outHeight);
-        (*vrTrgCursor)->AtkResNode.SetWidth(outWidth);
-        (*vrTrgCursor)->AtkResNode.SetHeight(outHeight);
-
-        // explanation of these numbers
-        // Some setup info:
-        // 1. The ↓ character output from GetTextDrawSize is always 1:1 with the
-        //    requested font. Font size 100 results in outWidth 100 and outHeight 100.
-        // 2. The anchor point for text fields are the upper left corner of the frame.
-        // 3. The hand-tuned position of the default font size 100 is x 90, y -23.
-        // 
-        // Adding the inverted delta offset (and div by 2 for x) correctly moves the ancor
-        // from upper left to bottom center. However I noticed that as the font scales
-        // up and down, the point of the arrow drifts slightly along the x and y. This
-        // is the reason for the * 1.10 and * 1.15. This corrects for the drift and keeps
-        // the point of the arrow exactly where it should be.
-
-        const float DriftOffset_X = 1.10f;
-        const float DriftOffset_Y = 1.15f;
-
-        short xpos = (short)(90 + ((100 - outWidth) / 2 * DriftOffset_X));
-        short ypos = (short)(-23 + (100 - outWidth) * DriftOffset_Y);
-        (*vrTrgCursor)->AtkResNode.SetPositionShort(xpos, ypos);
+        nameplateModifier.UpdateNamePlates(namePlate);
     }
 }
---
+++ FfxivVr/SkeletonModifier.cs
@@ -0,0 +1,101 @@
+﻿using FFXIVClientStructs.FFXIV.Client.Graphics.Render;
+using System.Collections.Generic;
+
+namespace FfxivVR;
+unsafe internal class SkeletonModifier(Logger logger)
+{
+    private readonly Logger logger = logger;
+
+    internal void HideHead(Skeleton* skeleton)
+    {
+        if (skeleton == null)
+        {
+            return;
+        }
+
+        var neckBone = GetNeckBone(skeleton);
+        if (neckBone != null)
+        {
+            for (int i = 0; i < skeleton->PartialSkeletonCount; i++)
+            {
+                var partial = skeleton->PartialSkeletons[i].GetHavokPose(0);
+                if (partial == null)
+                {
+                    continue;
+                }
+                if (neckBone.BoneIndex >= partial->LocalPose.Length)
+                {
+                    continue;
+                }
+                var transform = partial->LocalPose[(int)neckBone.BoneIndex!];
+                transform.Scale.X = 0.00001f;
+                transform.Scale.Y = 0.00001f;
+                transform.Scale.Z = 0.00001f;
+                transform.Scale.W = 0;
+                partial->LocalPose[(int)neckBone.BoneIndex!] = transform;
+
+                neckBone.Children.ForEach((c) =>
+                {
+                    if (c < partial->LocalPose.Length)
+                    {
+                        var transform = partial->LocalPose[(int)c];
+                        transform.Translation.X *= -1;
+                        transform.Translation.Y *= -1;
+                        transform.Translation.Z *= -1;
+                        transform.Translation.W *= -1;
+                        transform.Scale.X = 0.00001f;
+                        transform.Scale.Y = 0.00001f;
+                        transform.Scale.Z = 0.00001f;
+                        transform.Scale.W = 0.00001f;
+                        partial->LocalPose[(int)c] = transform;
+                    }
+                });
+            }
+        }
+        else
+        {
+            logger.Debug("Neck bone not found");
+        }
+    }
+
+    private Bone? neck = null;
+    private Bone? GetNeckBone(Skeleton* skeleton)
+    {
+        if (neck != null)
+        {
+            return neck;
+        }
+        if (skeleton->SkeletonResourceHandles != null && skeleton->SkeletonResourceHandles[0] != null)
+        {
+            int? index = null;
+            var children = new List<int>();
+            var havokSkeleton = skeleton->SkeletonResourceHandles[0]->HavokSkeleton;
+            for (int currentBone = 0; currentBone < havokSkeleton->Bones.Length; currentBone++)
+            {
+                var bone = havokSkeleton->Bones[currentBone];
+                var parent = havokSkeleton->ParentIndices[currentBone];
+                var name = bone!.Name!.String;
+                if (name == "j_kubi")
+                {
+                    index = currentBone;
+                }
+                if (parent == index)
+                {
+                    children.Add(currentBone);
+                }
+            }
+            if (index is int boneIndex)
+            {
+                neck = new Bone(boneIndex, children);
+            }
+        }
+        return neck;
+    }
+
+    class Bone(int boneIndex, List<int> children)
+    {
+        public List<int> Children = children;
+
+        public int BoneIndex { get; } = boneIndex;
+    }
+}
