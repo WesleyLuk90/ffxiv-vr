96ba7aba7645e8f868db8ce0b96a757c22cabde5
Wesley Luk <wesleyluk@gmail.com>
2024-10-28 20:40:57 -0700

    Set default world scale

--- FfxivVr/Configuration.cs
+++ FfxivVr/Configuration.cs
@@ -8,10 +8,9 @@ public class Configuration : IPluginConfiguration
 {
     public int Version { get; set; } = 0;
 
-    public float WorldScale = 1.0f;
+    public float WorldScale = 2.0f;
     public float UIDistance = 1.0f;
 
-    // the below exist just to make saving less cumbersome
     public void Save()
     {
         Plugin.PluginInterface.SavePluginConfig(this);
