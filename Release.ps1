2c4d6ebcbba6af17ffb986536b01854565f945c7
Wesley Luk <wesleyluk@gmail.com>
2024-11-01 12:47:22 -0700

    Release version 0.0.3

---
+++ Release.ps1
@@ -0,0 +1,17 @@
+$version = $args[0]
+
+if(!($version -match '\d+.\d+.\d+')) {
+    echo "Invalid version number"
+    exit
+}
+
+Remove-TypeData -ErrorAction Ignore System.Array 
+
+$repo = Get-Content 'PluginRepo/pluginmaster.json' -raw | ConvertFrom-Json
+$repo[0].AssemblyVersion = $version
+ConvertTo-Json $repo -depth 32| set-content 'PluginRepo/pluginmaster.json'
+
+git add .
+git commit -m "Release version $version"
+git tag v$version
+git push origin tag v$version
