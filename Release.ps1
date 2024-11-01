08c795062f3670f93e96e438b56289aad5f993a8
Wesley Luk <wesleyluk@gmail.com>
2024-11-01 13:23:24 -0700

    Test release script

--- Release.ps1
+++ Release.ps1
@@ -5,17 +5,24 @@ if(!($version -match '\d+.\d+.\d+')) {
     exit
 }
 
-Remove-TypeData -ErrorAction Ignore System.Array 
-
-$now = [int](Get-Date -UFormat %s -Millisecond 0)
-
-$repo = Get-Content 'PluginRepo/pluginmaster.json' -raw | ConvertFrom-Json
-$repo[0].AssemblyVersion = $version
-$repo[0].LastUpdated = $now
-ConvertTo-Json $repo -depth 32| set-content 'PluginRepo/pluginmaster.json'
+$xml = [xml](Get-Content -Path .\FfxivVr\FfxivVR.csproj)
+$xml.Project.PropertyGroup.Version = $version
+$xml.Save($xml)
 
 git add .
 git commit -m "Release version $version"
 git tag v$version
 git push origin master
 git push origin tag v$version
+
+Remove-TypeData -ErrorAction Ignore System.Array 
+$now = [int](Get-Date -UFormat %s -Millisecond 0)
+$repo = Get-Content 'PluginRepo/pluginmaster.json' -raw | ConvertFrom-Json
+$repo[0].AssemblyVersion = $version
+$repo[0].LastUpdated = $now
+ConvertTo-Json $repo -depth 32| set-content 'PluginRepo/pluginmaster.json'
+
+git add .
+git commit -m "Publish version $version"
+
+echo "Run `git push origin master` once release build is complete"
