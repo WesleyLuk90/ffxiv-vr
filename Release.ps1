a2a03abcc87f4ded57b4510da7237820a79ce7c4
Wesley Luk <wesleyluk@gmail.com>
2024-11-03 07:05:58 -0800

    Update release script

--- Release.ps1
+++ Release.ps1
@@ -9,23 +9,18 @@ $xml = [xml](Get-Content -Path .\FfxivVr\FfxivVR.csproj)
 $xml.Project.PropertyGroup.Version = $version
 $xml.Save(".\FfxivVr\FfxivVR.csproj")
 
-git add .
-git commit -m "Release version $version"
-git tag v$version
-git push origin master
-git push origin tag v$version
-
 Remove-TypeData -ErrorAction Ignore System.Array 
 $now = [int](Get-Date -UFormat %s -Millisecond 0)
-$repo = Get-Content 'PluginRepo/pluginmaster.json' -raw | ConvertFrom-Json
+$repo = Get-Content 'PluginRepo/next.json' -raw | ConvertFrom-Json
 $repo[0].AssemblyVersion = $version
 $repo[0].LastUpdated = $now
 $repo[0].DownloadLinkInstall = "https://github.com/WesleyLuk90/ffxiv-vr/releases/download/v$version/FfxivVR.zip"
 $repo[0].DownloadLinkTesting = "https://github.com/WesleyLuk90/ffxiv-vr/releases/download/v$version/FfxivVR.zip"
 $repo[0].DownloadLinkUpdate = "https://github.com/WesleyLuk90/ffxiv-vr/releases/download/v$version/FfxivVR.zip"
-ConvertTo-Json $repo -depth 32| set-content 'PluginRepo/pluginmaster.json'
+ConvertTo-Json $repo -depth 32| set-content 'PluginRepo/next.json'
 
 git add .
-git commit -m "Publish version $version"
-
-echo "Run `git push origin master` once release build is complete"
+git commit -m "Release version $version"
+git tag v$version
+git push origin master
+git push origin tag v$version
