522ec2784704f6dcc0ea66bb32bdcd85fbedc762
WesleyLuk90 <1383942+WesleyLuk90@users.noreply.github.com>
2025-03-29 13:53:13 -0700

    Add VR FPS Tracker (#59)

--- FfxivVr/math/GameClock.cs
+++ FfxivVr/math/GameClock.cs
@@ -1,15 +1,43 @@
+using Dalamud.Game.Gui.Dtr;
+using Dalamud.Plugin.Services;
+using System;
 using System.Diagnostics;
 
 namespace FfxivVR;
 
-public class GameClock
+public class GameClock(
+    IDtrBar dtrBar
+) : IDisposable
 {
-    private Stopwatch stopwatch = Stopwatch.StartNew();
+    private Stopwatch frameTimer = Stopwatch.StartNew();
+    private Stopwatch updateTimer = Stopwatch.StartNew();
+    private float frameTime = 0f;
+    private float smoothingFactor = 0.9f;
 
-    public float GetTicks()
+    private IDtrBarEntry? fpsEntry = dtrBar.Get("VR FPS", "VR FPS: N/A");
+
+    public void Dispose()
     {
-        var elapsed = stopwatch.Elapsed.TotalSeconds;
-        stopwatch.Restart();
-        return (float)elapsed;
+        fpsEntry?.Remove();
+        fpsEntry = null;
     }
+
+    public float MarkFrame()
+    {
+        var elapsed = (float)frameTimer.Elapsed.TotalSeconds;
+        frameTimer.Restart();
+
+        frameTime = frameTime * smoothingFactor + elapsed * (1 - smoothingFactor);
+        if (updateTimer.ElapsedMilliseconds > 1000)
+        {
+            var fps = 1f / frameTime;
+            updateTimer.Restart();
+            if (fpsEntry != null)
+            {
+                fpsEntry.Text = $"VR FPS: {fps:F1}";
+            }
+        }
+        return elapsed;
+    }
+
 }
