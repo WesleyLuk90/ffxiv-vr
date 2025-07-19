209a39903783ced623b22b8d2a1e08e492a8d894
WesleyLuk90 <1383942+WesleyLuk90@users.noreply.github.com>
2025-07-19 12:15:40 -0700

    Update offset for fixed head position (#77)

--- FfxivVr/game/structs/InternalCharacter.cs
+++ FfxivVr/game/structs/InternalCharacter.cs
@@ -7,5 +7,5 @@ namespace FfxivVR;
 public unsafe struct InternalCharacter
 {
     public static InternalCharacter* FromCharacter(Character* character) => (InternalCharacter*)character;
-    [FieldOffset(308)] public float FixHeadPosition;
+    [FieldOffset((int)(0x1C9A33DB174 - 0x1c9a33db050))] public float FixHeadPosition;
 }
