b94b060929a359278dabd62db215d02bf7fbc372
mlgi <migi@miguelchristian.com>
2026-04-13 22:20:12 -0700

    fix(#91): update to new dalamud RenderDrawDataInternal

--- FfxivVr/game/DalamudRenderer.cs
+++ FfxivVr/game/DalamudRenderer.cs
@@ -18,25 +18,35 @@ public unsafe class DalamudRenderer
     public void Initialize()
     {
         var interfaceManager = Service<InterfaceManager>.GetNullable() ?? throw new Exception("Failed to get InterfaceManager");
-        var backend = interfaceManager.Backend as Dx11Win32Backend ?? throw new Exception("Failed to get Dx11Win32Backend"); ;
+        var backend = interfaceManager.Backend as Dx11Win32Backend ?? throw new Exception("Failed to get Dx11Win32Backend");
         renderer = backend.Renderer as Dx11Renderer ?? throw new Exception("Failed to get Dx11Renderer");
 
         var assembly = Assembly.GetAssembly(typeof(Dx11Renderer)) ?? throw new Exception("Failed to get Assembly");
         var type = assembly.GetType("Dalamud.Interface.ImGuiBackend.Renderers.Dx11Renderer") ?? throw new Exception("Failed to get Dx11Renderer type");
-        renderDrawDataInternalMethod = type.GetRuntimeMethods().Where(m => m.Name == "RenderDrawDataInternal").First();
+
+        var methods = type.GetRuntimeMethods().Where(m => m.Name == "RenderDrawDataInternal").ToList();
+
+        renderDrawDataInternalMethod = methods.First();
     }
 
-    internal void Render(ID3D11RenderTargetView* renderTargetView)
+    internal void Render(ID3D11Texture2D* renderTargetTexture, ID3D11RenderTargetView* renderTargetView)
     {
-        RenderDrawDataInternal(renderTargetView, ImGui.GetDrawData(), false);
+        // Pass the texture down into the internal method
+        RenderDrawDataInternal(renderTargetTexture, renderTargetView, ImGui.GetDrawData(), false);
     }
 
     private void RenderDrawDataInternal(
-       ID3D11RenderTargetView* renderTargetView,
-       ImDrawDataPtr drawData,
-       bool clearRenderTarget)
+        ID3D11Texture2D* renderTargetTexture,
+        ID3D11RenderTargetView* renderTargetView,
+        ImDrawDataPtr drawData,
+        bool clearRenderTarget)
     {
-        renderDrawDataInternalMethod?.Invoke(renderer, [(IntPtr)renderTargetView, drawData, clearRenderTarget]);
+        renderDrawDataInternalMethod?.Invoke(renderer, [
+            (IntPtr)renderTargetTexture,
+            (IntPtr)renderTargetView,
+            drawData,
+            clearRenderTarget
+        ]);
     }
 
 }
