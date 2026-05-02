dbd33075e25b8f8f1eaa781f6d32d550000a6ca3
WesleyLuk90 <1383942+WesleyLuk90@users.noreply.github.com>
2026-05-02 12:14:43 -0700

    Fix version handling again (#105)

--- release/bump-version.ps1
+++ release/bump-version.ps1
@@ -1,23 +1,27 @@
-param(
-    [switch]$DryRun
-)
-
 $xml = [xml](Get-Content -Path .\FfxivVr\FfxivVR.csproj)
 
 $currentVersion = [version]$xml.Project.PropertyGroup.Version
 $nextVersion = "{0}.{1}.{2}" -f $currentVersion.Major, $currentVersion.Minor, ($currentVersion.Build + 1)
 $versionString = "v$nextVersion"
 
-echo "Bumping version from $currentVersion to $nextVersion"
-
-if (-not $DryRun) {
-    $xml.Project.PropertyGroup.Version = $nextVersion
-    $xml.Save(".\FfxivVr\FfxivVR.csproj")
-
-    Remove-TypeData -ErrorAction Ignore System.Array
-    $repo = Get-Content 'PluginRepo/pluginmaster.json' -raw | ConvertFrom-Json
-    $repo[0].AssemblyVersion = $nextVersion
-    ConvertTo-Json $repo -depth 32 | set-content 'PluginRepo/pluginmaster.json'
+$changeLog = git log --pretty=format:"# %s%n%b" "v$currentVersion"..HEAD --invert-grep --grep="Publish Version"
+if (!$changeLog) {
+    Write-Host "Changelog was empty, exiting"
+    Exit 1
 }
 
-echo $versionString
+Write-Host "Bumping version from $currentVersion to $nextVersion"
+Write-Host "=== Change Log ==="
+Write-Host $changeLog
+
+$xml.Project.PropertyGroup.Version = $nextVersion
+$xml.Save(".\FfxivVr\FfxivVR.csproj")
+
+Remove-TypeData -ErrorAction Ignore System.Array
+$repo = Get-Content 'PluginRepo/pluginmaster.json' -raw | ConvertFrom-Json
+$repo[0].AssemblyVersion = $nextVersion
+ConvertTo-Json $repo -depth 32 | set-content 'PluginRepo/pluginmaster.json'
+
+[IO.File]::WriteAllLines("release/changelog.txt", $changeLog)
+
+echo "VERSION_STRING=$versionString" >> $env:GITHUB_OUTPUT
--- release/finalize-release.ps1
+++ release/finalize-release.ps1
@@ -2,28 +2,7 @@ param(
     [string]$VersionString
 )
 
-if ($VersionString -and $VersionString -notmatch '^v') {
-    $VersionString = "v$VersionString"
-}
-
 $nextVersion = $VersionString -replace '^v', ''
-$currentVersion = [version](Select-String -Path ".\FfxivVr\FfxivVR.csproj" -Pattern '<Version>(.*?)</Version>' | ForEach-Object { $_.Matches[0].Groups[1].Value })
-$changeLog = git log --pretty=format:"# %s%n%b" $currentVersion..HEAD --invert-grep --grep="Publish Version"
-$fixes = $changeLog | Select-String "#\d+" | % { "Closes " + $_.Matches.Value }
-$releaseMessage = "Publish Version $nextVersion`n" + [string]::Join("`n", $fixes)
-
-if (!$changeLog) {
-    echo "Changelog was empty, exiting"
-    Exit 1
-}
-
-echo "Deploying $VersionString"
-echo "=== Change Log ==="
-echo $changeLog
-echo "=== Release Message ==="
-echo $releaseMessage
-
-[IO.File]::WriteAllLines("release/changelog.txt", $changeLog)
 
 $now = [int](Get-Date -UFormat %s -Millisecond 0)
 $repo = Get-Content 'PluginRepo/pluginmaster.json' -raw | ConvertFrom-Json
@@ -36,5 +15,5 @@ ConvertTo-Json $repo -depth 32 | set-content 'PluginRepo/pluginmaster.json'
 git config --global user.email "1383942+WesleyLuk90@users.noreply.github.com"
 git config --global user.name "Release Bot"
 git add .
-git commit -m "$releaseMessage"
+git commit -m "Publish Version $nextVersion"
 git tag $VersionString
